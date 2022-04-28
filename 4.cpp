4841e21d0cbe63b05cabf9211b6a6f459008fd44
DlililS <ladus2004daria@gmail.com>
2022-04-29 02:04:48 +0300

    Initial commit

---
+++ 4.cpp
@@ -0,0 +1,17 @@
+﻿#include <cmath>
+#include <iostream>
+using namespace std;
+int main() {
+    double a = 4.8;
+    double b = -7.9;
+    double n;
+    if (a >= b) { n = pow(a - b, (0.33)); }
+    else if (a < b) n = (pow(a, 2) + ((a - b) / (std::sin(a * b))));
+    if (n < b) {
+        cout << ((n + a) / (-b) + sqrt(pow(std::sin(a), 2) - cos(n)));
+    }
+    else if (n == b)
+        cout << (tan(n * a) + pow(b, 2));
+    else if (n > b)
+        cout << (pow(b, 3) + n * pow(a, 2));
+}
